28bbda448531516b209f8e1840e96b23158160a5
Thorne <ExistentialEnso@users.noreply.github.com>
2024-12-09 17:02:57 -0500

    Icon support

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,36 @@
 import configuration from "./configuration.json"
+import { FaXTwitter } from "react-icons/fa6"
+import { FaLinkedin } from "react-icons/fa"
+import { FaReddit } from "react-icons/fa"
+import { FaBluesky } from "react-icons/fa6"
+import { FaGithub } from "react-icons/fa"
+
+const getIcon = (slug: string) => {
+    switch (slug) {
+        case "x":
+            return <FaXTwitter className="w-8 h-8 mr-2" />
+        case "linkedin":
+            return <FaLinkedin className="w-8 h-8 mr-2" />
+        case "reddit":
+            return <FaReddit className="w-8 h-8 mr-2" />
+        case "bluesky":
+            return <FaBluesky className="w-8 h-8 mr-2" />
+        case "github":
+            return <FaGithub className="w-8 h-8 mr-2" />
+        default:
+            return <></>
+    }
+}
 
 export default function Home() {
     return(
         <div className="flex flex-col items-center justify-center h-screen">
-            <h1 className="text-4xl font-bold font-mono">{configuration.title}</h1>
-            <ul>
+            <h1 className="text-4xl font-bold font-mono mb-2">{configuration.title}</h1>
+            <div className="w-full p-4">
                 {configuration.links.map((link) => (
-                    <a className="text-lg text-center font-mono block my-4 bg-slate-900 text-white p-2 rounded-md w-[500px]" href={link.url} key={link.name}>{link.name}</a>
+                    <a className="text-xl text-center font-mono block mx-auto my-4 bg-slate-800 hover:bg-slate-700 text-white p-2 rounded-md w-full md:w-[500px]" href={link.url} key={link.name}><div className="flex items-center justify-center">{getIcon(link.icon ?? "")} {link.name}</div></a>
                 ))}
-            </ul>
+            </div>
         </div>
     )
 }
